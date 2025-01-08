dfda954fb22fb28b921f0c85cfbef2d0ac3a87e7
Kirill Sandow <amphyxs@gmail.com>
2025-01-08 11:52:16 +0300

    lint(app): fix SonarQube issues #2

--- stage-3/src/main/java/com/par/parapp/service/InventoryService.java
+++ stage-3/src/main/java/com/par/parapp/service/InventoryService.java
@@ -1,5 +1,14 @@
 package com.par.parapp.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.par.parapp.dto.ItemResponse;
 import com.par.parapp.exception.ResourceNotFoundException;
 import com.par.parapp.model.Game;
@@ -9,15 +18,10 @@ import com.par.parapp.model.User;
 import com.par.parapp.repository.InventoryRepository;
 import com.par.parapp.repository.ItemRepository;
 import com.par.parapp.repository.ShopRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
@@ -47,52 +51,19 @@ public class InventoryService {
                         case "Обычная" -> commonItem = item;
                         case "Редкая" -> rareItem = item;
                         case "Легендарная" -> legendaryItem = item;
-                        default -> {
-                        }
                     }
                 }
             }
         }
 
-        double rnd = Math.random();
-        System.out.println(rnd);
-
-        if (rnd > 0.80) {
-
-            if (rnd > 0.95) {
-
-                if (legendaryItem != null) {
-                    inventoryRepository.save(new Inventory(user, legendaryItem, 0));
-                    return 1;
-                } else if (rareItem != null) {
-                    inventoryRepository.save(new Inventory(user, rareItem, 0));
-                    return 1;
-                } else if (commonItem != null) {
-                    inventoryRepository.save(new Inventory(user, commonItem, 0));
-                    return 1;
-                }
-
-            } else if (rareItem != null) {
-                inventoryRepository.save(new Inventory(user, rareItem, 0));
-                return 1;
-            } else if (commonItem != null) {
-                inventoryRepository.save(new Inventory(user, commonItem, 0));
-                return 1;
-            }
-        } else if (commonItem != null) {
-            inventoryRepository.save(new Inventory(user, commonItem, 0));
-            return 1;
-        }
-        return 0;
+        return giveawayRandomItemToUser(user, legendaryItem, rareItem, commonItem);
     }
 
     public Optional<Inventory> getByItemId(String userLogin, Long itemId) {
         Page<Inventory> allInventory = inventoryRepository.findAllByUser(userLogin, Pageable.unpaged())
                 .orElseThrow(ResourceNotFoundException::new);
         List<Inventory> allInventoryData = allInventory.getContent();
-        return allInventoryData.stream().filter(inventory -> {
-            return inventory.getItem().getId().equals(itemId);
-        }).findFirst();
+        return allInventoryData.stream().filter(inventory -> inventory.getItem().getId().equals(itemId)).findFirst();
     }
 
     public List<ItemResponse> getAllItems(String userLogin, int page, int size) {
@@ -102,12 +73,10 @@ public class InventoryService {
                 .orElseThrow(ResourceNotFoundException::new);
         List<Inventory> allInventoryData = allInventory.getContent();
         List<ItemResponse> itemResponses = new ArrayList<>();
-        allInventoryData.forEach(inventory -> {
-            itemResponses.add(new ItemResponse(inventory.getItem().getItemUrl(),
-                    inventory.getItem().getName(), inventory.getItem().getGame().getName(),
-                    inventory.getItem().getRarity(), inventory.getAmount(),
-                    shopRepository.getGameShopPictureByGameName(inventory.getItem().getGame().getName())));
-        });
+        allInventoryData.forEach(inventory -> itemResponses.add(new ItemResponse(inventory.getItem().getItemUrl(),
+                inventory.getItem().getName(), inventory.getItem().getGame().getName(),
+                inventory.getItem().getRarity(), inventory.getAmount(),
+                shopRepository.getGameShopPictureByGameName(inventory.getItem().getGame().getName()))));
 
         return itemResponses;
     }
@@ -122,9 +91,7 @@ public class InventoryService {
                 .ifPresentOrElse(inventory -> {
                     inventory.setAmount(inventory.getAmount() + 1);
                     inventoryRepository.save(inventory);
-                }, () -> {
-                    inventoryRepository.save(new Inventory(user, item, 1));
-                });
+                }, () -> inventoryRepository.save(new Inventory(user, item, 1)));
     }
 
     public void removeItemFromSeller(User user, Item item) {
@@ -142,6 +109,38 @@ public class InventoryService {
                         inventoryRepository.save(inventory);
                     }
                 });
+    }
+
+    private int giveawayRandomItemToUser(User user, Item legendaryItem, Item rareItem, Item commonItem) {
+        double rnd = Math.random();
+        log.info("Item giveaway number: " + rnd);
+
+        if (rnd > 0.80) {
+            if (rnd > 0.95) {
+                if (legendaryItem != null) {
+                    inventoryRepository.save(new Inventory(user, legendaryItem, 1));
+                    return 1;
+                } else if (rareItem != null) {
+                    inventoryRepository.save(new Inventory(user, rareItem, 1));
+                    return 1;
+                } else if (commonItem != null) {
+                    inventoryRepository.save(new Inventory(user, commonItem, 1));
+                    return 1;
+                }
+
+            } else if (rareItem != null) {
+                inventoryRepository.save(new Inventory(user, rareItem, 1));
+                return 1;
+            } else if (commonItem != null) {
+                inventoryRepository.save(new Inventory(user, commonItem, 1));
+                return 1;
+            }
+        } else if (commonItem != null) {
+            inventoryRepository.save(new Inventory(user, commonItem, 1));
+            return 1;
+        }
+
+        return 0;
     }
 
 }
